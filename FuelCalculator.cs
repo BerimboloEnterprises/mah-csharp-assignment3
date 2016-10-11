2ad8fb78576162ff8f3adbfb1b4c3eebfbbd613c
Marcus Lindstedt <marcus.t.lindstedt@gmail.com>
2016-10-11 10:13:29 +0200

    Initial commit for assignment 3

---
+++ FuelCalculator.cs
@@ -0,0 +1,116 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace Assignment3
+{
+    class FuelCalculator
+    {
+        private double currentReading;
+        private double previousReading;
+        private double fuelAmount;
+        private double unitPrice;
+        private double consumption;
+        private double distanceTraveled;
+
+        public FuelCalculator()
+        {
+
+        }
+
+        public double GetFuelAmount()
+        {
+            return fuelAmount;
+        }
+
+        public double GetCurrentReading()
+        {
+            return currentReading;
+        }
+
+        public double GetPreviousReading()
+        {
+            return previousReading;
+        }
+
+        public double GetUnitPrice()
+        {
+            return unitPrice;
+        }
+
+        public void SetCurrentReading(double reading)
+        {
+            currentReading = reading;
+        }
+
+        public void SetFuelAmount(double fuel)
+        {
+            fuelAmount = fuel;
+        }
+
+        public void SetPreviousReading(double reading)
+        {
+            previousReading = reading;
+        }
+
+        public void SetUnitPrice(double price)
+        {
+            unitPrice = price;
+        }
+
+        public bool ValidateOdomoterValues()
+        {
+            return true;
+        }
+
+        public double CalculateConsumptionLiterPerKM()
+        {
+            CalculateDistanceTraveled();
+            consumption = fuelAmount / distanceTraveled;
+
+            return consumption;
+        }
+
+        public double CalculateConsumptionKMPerLiter()
+        {
+            CalculateDistanceTraveled();
+            consumption = distanceTraveled / fuelAmount;
+
+            return consumption;
+        }
+
+        public double CalculateDistanceTraveled()
+        {
+            distanceTraveled = (currentReading - previousReading);
+
+            return distanceTraveled;
+        }
+
+        public double CalculateConsumptionLiterPerImperialMile()
+        {
+            const double kmToMileFactor = 0.621371192;
+            consumption = CalculateDistanceTraveled() / kmToMileFactor;
+
+            return consumption;
+        }
+
+        public double CalculateConsumptionLiterPerMetricMile()
+        {
+            consumption = CalculateConsumptionKMPerLiter() * 10;
+
+            return consumption;
+        }
+
+        public double CalculateCostPerKmDriven()
+        {
+            consumption = CalculateConsumptionKMPerLiter() * unitPrice;
+
+            return consumption;
+        }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,46 @@
+﻿#region Using Statements
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+#endregion
+
+namespace Assignment3
+{
+    public partial class MainForm : Form
+    {
+        private FuelCalculator carMileage = new FuelCalculator();
+        private BodyMassIndexCalculator bmiCalc = new BodyMassIndexCalculator();
+
+        private string name = String.Empty;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeGUI();
+        }
+
+        private void InitializeGUI()
+        {
+            //Set all labels to default value or empty depending on suitability
+        }
+
+      
+        private void UpdateGUIFuel()
+        {
+
+        }
+
+        private void UpdateGUIBRM()
+        {
+
+        }
+    }
+}
